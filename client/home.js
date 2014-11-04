e85692e1499c9a0b5de306744b52c36135ba9a7f
Joy Pratt <joympratt@gmail.com>
2014-11-04 12:42:51 -0600

    add section titles

--- client/home.js
+++ client/home.js
@@ -4,5 +4,39 @@
   angular.module('portfolio')
   .controller('HomeCtrl', ['$scope', function($scope){
     $scope.title = 'Joy Pratt';
+
+    $scope.home = true;
+    $scope.about = false;
+    $scope.skills = false;
+    $scope.projects = false;
+
+    $scope.home = function(){
+      $scope.home = true;
+      $scope.about = false;
+      $scope.skills = false;
+      $scope.projects = false;
+    };
+
+    $scope.about = function(){
+      $scope.home = false;
+      $scope.about = true;
+      $scope.skills = false;
+      $scope.projects = false;
+    };
+
+    $scope.skills = function(){
+      $scope.home = false;
+      $scope.about = false;
+      $scope.skills = true;
+      $scope.projects = false;
+    };
+
+    $scope.projects = function(){
+      $scope.home = false;
+      $scope.about = false;
+      $scope.skills = false;
+      $scope.projects = true;
+    };
+
   }]);
 })();
